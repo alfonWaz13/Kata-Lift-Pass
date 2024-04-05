7795fe3915e92ee919925b826886232b1b5c6684
alfonWaz13 <alfonwaz13@icloud.com>
2024-04-05 16:46:58 +0200

    style: apply black in src and test

--- test/acceptance/test_lift_pass.py
+++ test/acceptance/test_lift_pass.py
@@ -12,35 +12,47 @@ class TestLiftPass:
     def test_1jour_general_price_is_35(self) -> None:
         client = app.test_client()
         response = client.get("/prices", query_string={"type": "1jour"})
-        assert response.json == {'cost': 35}
+        assert response.json == {"cost": 35}
 
     def test_1jour_cost_is_35_if_is_holiday(self) -> None:
         client = app.test_client()
-        response = client.get("/prices", query_string={"type": "1jour", "date": "2019-02-25"})
-        assert response.json == {'cost': 35}
+        response = client.get(
+            "/prices", query_string={"type": "1jour", "date": "2019-02-25"}
+        )
+        assert response.json == {"cost": 35}
 
-    def test_1jour_cost_is_35_percent_reduced_if_is_not_holiday_and_is_monday(self) -> None:
+    def test_1jour_cost_is_35_percent_reduced_if_is_not_holiday_and_is_monday(
+        self,
+    ) -> None:
         client = app.test_client()
-        response = client.get("/prices", query_string={"type": "1jour", "date": "2024-02-26"})
-        assert response.json == {'cost': 23}
+        response = client.get(
+            "/prices", query_string={"type": "1jour", "date": "2024-02-26"}
+        )
+        assert response.json == {"cost": 23}
 
-    @pytest.mark.parametrize("age, expected_price", [(5, 0),
-                                                     (10, 25),
-                                                     (65, 27),
-                                                     (16, 35)])
-    def test_1jour_cost_with_age_matches_expected_price(self, age: int, expected_price: int):
+    @pytest.mark.parametrize(
+        "age, expected_price", [(5, 0), (10, 25), (65, 27), (16, 35)]
+    )
+    def test_1jour_cost_with_age_matches_expected_price(
+        self, age: int, expected_price: int
+    ):
         client = app.test_client()
-        response = client.get("/prices", query_string={"type": "1jour", "age": f"{age}"})
-        assert response.json == {'cost': expected_price}
+        response = client.get(
+            "/prices", query_string={"type": "1jour", "age": f"{age}"}
+        )
+        assert response.json == {"cost": expected_price}
 
     def test_night_general_price_is_0(self) -> None:
         client = app.test_client()
         response = client.get("/prices", query_string={"type": "night"})
-        assert response.json == {'cost': 0}
+        assert response.json == {"cost": 0}
 
-    @pytest.mark.parametrize("age, expected_price", [(10, 19),
-                                                     (65, 8)])
-    def test_night_cost_with_age_matches_expected_price(self, age: int, expected_price: int):
+    @pytest.mark.parametrize("age, expected_price", [(10, 19), (65, 8)])
+    def test_night_cost_with_age_matches_expected_price(
+        self, age: int, expected_price: int
+    ):
         client = app.test_client()
-        response = client.get("/prices", query_string={"type": "night", "age": f"{age}"})
-        assert response.json == {'cost': expected_price}
+        response = client.get(
+            "/prices", query_string={"type": "night", "age": f"{age}"}
+        )
+        assert response.json == {"cost": expected_price}
