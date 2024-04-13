d764c3a8ce9fe009a4a010d80a4b6018459e67bd
alfonWaz13 <alfonwaz13@icloud.com>
2024-04-13 12:46:10 +0200

    refactor(prices): get price feature extracted to method

--- src/prices.py
+++ src/prices.py
@@ -22,67 +22,70 @@ connection = None
 
 @app.route("/prices", methods=["GET", "PUT"])
 def prices() -> Dict[str, int]:
-    res = {}
     global connection
     if connection is None:
         connection = create_lift_pass_db_connection(connection_options)
     if request.method == "PUT":
         return _add_price(connection)
-    else:
-        cursor = connection.cursor()
-        cursor.execute(
-            f"SELECT cost FROM base_price " + "WHERE type = ? ", (request.args["type"],)
-        )
-        row = cursor.fetchone()
-        result = {"cost": row[0]}
-        age = request.args.get("age")
-        if age and int(age) < 6:
-            res["cost"] = 0
-        else:
-            if "type" in request.args and request.args["type"] != "night":
-                cursor = connection.cursor()
-                cursor.execute("SELECT * FROM holidays")
-                is_holiday = False
-                reduction = 0
-                for row in cursor.fetchall():
-                    holiday = row[0]
-                    if "date" in request.args:
-                        d = datetime.fromisoformat(request.args["date"])
-                        if (
-                            d.year == holiday.year
-                            and d.month == holiday.month
-                            and holiday.day == d.day
-                        ):
-                            is_holiday = True
-                if (
-                    not is_holiday
-                    and "date" in request.args
-                    and datetime.fromisoformat(request.args["date"]).weekday() == 0
-                ):
-                    reduction = 35
+    return _get_price(connection)
 
-                # TODO: apply reduction for others
-                if age and int(age) < 15:
-                    res["cost"] = math.ceil(result["cost"] * 0.7)
+
+def _get_price(connection: Connection) -> dict:
+    res = {}
+    cursor = connection.cursor()
+    cursor.execute(
+        f"SELECT cost FROM base_price " + "WHERE type = ? ", (request.args["type"],)
+    )
+    row = cursor.fetchone()
+    result = {"cost": row[0]}
+    age = request.args.get("age")
+    if age and int(age) < 6:
+        res["cost"] = 0
+    else:
+        if "type" in request.args and request.args["type"] != "night":
+            cursor = connection.cursor()
+            cursor.execute("SELECT * FROM holidays")
+            is_holiday = False
+            reduction = 0
+            for row in cursor.fetchall():
+                holiday = row[0]
+                if "date" in request.args:
+                    d = datetime.fromisoformat(request.args["date"])
+                    if (
+                        d.year == holiday.year
+                        and d.month == holiday.month
+                        and holiday.day == d.day
+                    ):
+                        is_holiday = True
+            if (
+                not is_holiday
+                and "date" in request.args
+                and datetime.fromisoformat(request.args["date"]).weekday() == 0
+            ):
+                reduction = 35
+
+            # TODO: apply reduction for others
+            if age and int(age) < 15:
+                res["cost"] = math.ceil(result["cost"] * 0.7)
+            else:
+                if not age:
+                    cost = result["cost"] * (1 - reduction / 100)
+                    res["cost"] = math.ceil(cost)
                 else:
-                    if not age:
-                        cost = result["cost"] * (1 - reduction / 100)
+                    if age and int(age) > 64:
+                        cost = result["cost"] * 0.75 * (1 - reduction / 100)
                         res["cost"] = math.ceil(cost)
                     else:
-                        if age and int(age) > 64:
-                            cost = result["cost"] * 0.75 * (1 - reduction / 100)
-                            res["cost"] = math.ceil(cost)
-                        else:
-                            cost = result["cost"] * (1 - reduction / 100)
-                            res["cost"] = math.ceil(cost)
-            else:
-                if age and int(age) >= 6:
-                    if int(age) > 64:
-                        res["cost"] = math.ceil(result["cost"] * 0.4)
-                    else:
-                        res.update(result)
+                        cost = result["cost"] * (1 - reduction / 100)
+                        res["cost"] = math.ceil(cost)
+        else:
+            if age and int(age) >= 6:
+                if int(age) > 64:
+                    res["cost"] = math.ceil(result["cost"] * 0.4)
                 else:
-                    res["cost"] = 0
+                    res.update(result)
+            else:
+                res["cost"] = 0
 
     return res
 
