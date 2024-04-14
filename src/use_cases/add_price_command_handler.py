c2bb54910b4708c211bf1ce53c36d11db33e3420
alfonWaz13 <alfonwaz13@icloud.com>
2024-04-14 17:18:27 +0200

    feat(prices): add AddPriceCommandHandler class

--- src/use_cases/add_price_command_handler.py
+++ src/use_cases/add_price_command_handler.py
@@ -1,7 +1,15 @@
 from dataclasses import dataclass
 
+from pymysql import Connection
+
 
 @dataclass
 class AddPriceCommand:
     lift_pass_type: str
     cost: float
+
+
+class AddPriceCommandHandler:
+
+    def __init__(self, connection: Connection) -> None:
+        self.connection = connection
