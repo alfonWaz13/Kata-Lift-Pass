5ed82fd13eca94afa8bf0ab30306f003662d8ce9
alfonWaz13 <alfonwaz13@icloud.com>
2024-04-14 17:21:05 +0200

    feat(prices): add execute method to AddPriceCommandHandler class

--- src/use_cases/add_price_command_handler.py
+++ src/use_cases/add_price_command_handler.py
@@ -6,10 +6,20 @@ from pymysql import Connection
 @dataclass
 class AddPriceCommand:
     lift_pass_type: str
-    cost: float
+    lift_pass_cost: float
 
 
 class AddPriceCommandHandler:
 
     def __init__(self, connection: Connection) -> None:
         self.connection = connection
+
+    def execute(self, command: AddPriceCommand) -> dict:
+
+        cursor = self.connection.cursor()
+        cursor.execute(
+            "INSERT INTO `base_price` (type, cost) VALUES (?, ?) "
+            + "ON DUPLICATE KEY UPDATE cost = ?",
+            (command.lift_pass_type, command.lift_pass_cost, command.lift_pass_cost),
+        )
+        return {}
